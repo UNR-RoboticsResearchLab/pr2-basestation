cbff01e91a028e31241445a747902d9c15d7520a
TheDash <dash@clearpathrobotics.com>
2015-11-04 12:27:53 -0500

    Added support for any mac address. All even macs are c2, all odd are c1.

--- genc1.py
+++ genc1.py
@@ -7,8 +7,8 @@ outstring += "# exeeds 1024 characters which the parser doesn't seem to like\n\n
 
 outstring += "dhcp-range=net:c1,10.68.0.1,10.68.0.1,2m\n"
 for i in range(1,256,2):
-    outstring += "dhcp-mac=c1,00:23:8b:*:*:%.2x\n"%i
+    outstring += "dhcp-mac=c1,*:*:*:*:*:%.2x\n"%i
 for i in range(1,256,2):
-    outstring += "dhcp-mac=c1,00:1e:68:*:*:%.2x\n"%i
+    outstring += "dhcp-mac=c1,*:*:*:*:*:%.2x\n"%i
 
 print outstring
